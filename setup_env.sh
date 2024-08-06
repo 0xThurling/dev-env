b00d2c74093a79ca2d8933d572e9d9a6aa9b2547
Jacques Thurling <j.thurling@capital-sa.com>
2024-08-06 16:38:35 +0200

    Update .gitignore and resync repository

--- setup_env.sh
+++ setup_env.sh
@@ -8,3 +8,14 @@
 # Version:      0.1
 # Usage:        ./setup_env.sh [options]
 #==============================================================================
+
+#main script logic
+main() {
+	if [[ ! -d "./dev-env-mount" ]]; then
+		echo "Mount doesn't exist. Creating..."
+		mkdir -p "dev-env-mount"
+	fi
+}
+
+#execute main function
+main

6fecdf6dc644eebd52aab674d64a0ebc6dfae6dc
Jacques Thurling <j.thurling@capital-sa.com>
2024-08-06 16:28:37 +0200

    Update .gitignore and resync repository

---
+++ setup_env.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#==============================================================================
+# Title:        setup_env.sh
+# Description:  Setup new containerised dev environment
+# Author:       Doge
+# Date:         2024-07-06
+# Version:      0.1
+# Usage:        ./setup_env.sh [options]
+#==============================================================================

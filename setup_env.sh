15b34d196b7bd1d7ff602b3687ec3784b5b91189
Jacques Thurling <j.thurling@capital-sa.com>
2024-08-07 09:35:37 +0200

    Updated .gitignore

--- setup_env.sh
+++ setup_env.sh
@@ -16,11 +16,25 @@ main() {
 		mkdir -p "dev-env-mount"
 	fi
 
+	if [[ ! -f "./docker-compose.yml" ]]; then
+		cp ./docker-compose.setup.yml ./docker-compose.yml
+	else
+		rm ./docker-compose.yml
+
+		cp ./docker-compose.setup.yml ./docker-compose.yml
+	fi
+
 	echo "Enter your GitHub name: "
 	read github_name
 
 	echo "Enter your GitHub email: "
 	read github_email
+
+	# Perform the replacement using sed
+	sed -i '' "s/{GIT_USER_NAME}/$github_name/g" "./docker-compose.yml"
+	sed -i '' "s/{GIT_USER_EMAIL}/$github_email/g" "./docker-compose.yml"
+
+	docker compose build --no-cache doge-local-dev-environment
 }
 
 #execute main function
